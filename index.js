c9ccfcf2e36648d4760dfe27173420052c5a0d46
christopher boucher <chrismichb@yahoo.com>
2021-04-15 23:35:12 -0400

    created forEach loop to search array

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const validateLineup = (lineup) => {
-
+  lineup.forEach(function(value) {
+    console.log(value.salary)
+  });
 }
 
+//const totalSalary = (value) => {
+//  
+//}
 module.exports = validateLineup
