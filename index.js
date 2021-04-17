bbf4b7b2e7e05d9755f2a400503bf5e831188130
christopher boucher <chrismichb@yahoo.com>
2021-04-16 21:07:17 -0400

    Added playerPosition function

--- index.js
+++ index.js
@@ -6,16 +6,23 @@ const validateLineup = (lineup) => {
   }
 }
 
+const playerPosition = (lineup) => {
+  
+}
+
 const totalSalary = (lineup) => {
   let amount = []
-  lineup.forEach(function(value) {
+
+  lineup.forEach(function (value) {
     amount.push(value.salary)
-  });
-  const amountSum = amount.reduce((total, money) => total + money, 0);
+  })
+  const amountSum = amount.reduce((total, money) => total + money, 0)
+
   if (amountSum <= 45000) {
     return true
   } else {
     return false
   }
 }
+
 module.exports = validateLineup
